7347be017a0b25dd80697c536597693b0d487616
Sandis Kaģis <s20kagisand@venta.lv>
2024-05-20 19:39:38 +0300

    New: CustomerAsCompany class

---
+++ src/main/java/lv/venta/model/CustomerAsCompany.java
@@ -0,0 +1,47 @@
+package lv.venta.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "CUSTOMER_AS_COMPANY")
+@Entity
+public class CustomerAsCompany extends AbstractCustomer{
+
+	@NotNull
+	@Pattern(regexp = "[A-Za-z0-9 ]{2,60}", message = "Invalid title")
+	@Column(name = "TITLE")
+	private String title;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Za-z0-9 ]{2,60}", message = "Invalid address")
+	@Column(name = "COMPANY_REG_NO")
+    private String companyRegNo;
+	
+	
+	public CustomerAsCompany(Address address, String phoneNo, String title, String companyRegNo) {
+		super(address, phoneNo);
+        setTitle(title);
+        setCompanyRegNo(companyRegNo);
+        setCustomerCode();
+	}
+
+
+	@Override
+	public void setCustomerCode() {
+		super.customerCode = super.getCID() + "_company_" + this.companyRegNo;
+	}
+	
+}
